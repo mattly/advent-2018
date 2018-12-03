3164ef0c013038e6f3474417aee819fd61204dc3
Matthew Lyon <matthew@lyonheart.us>
2018-12-03 12:24:45 -0800

    solve 3.1

--- clojure/src/d03.clj
+++ clojure/src/d03.clj
@@ -10,3 +10,18 @@
               (map (fn parse-nums [i] (Integer/parseInt i)))
               (zipmap [:id :x :y :w :h])))
        (help/input "03.txt")))
+
+;; validating -- what are our max coordinates?
+(->> input (map #(+ (:x %) (:w %))) (apply max))
+(->> input (map #(+ (:y %) (:h %))) (apply max))
+
+;; find contested coordinates
+(->> input
+     (mapcat (fn coordinates-for [{:keys [x y w h]}]
+               (for [xp (range w)
+                     yp (range h)]
+                 [(+ x xp) (+ y yp)])))
+     frequencies
+     vals
+     (filter (fn find-contested [freq] (< 1 freq)))
+     count)
