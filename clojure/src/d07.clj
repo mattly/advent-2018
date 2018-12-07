a37ddbda9dbf231d257ba4248d57975dea7d0a45
Matthew Lyon <matthew@lyonheart.us>
2018-12-07 07:11:24 -0800

    day 7 p 2

--- clojure/src/d07.clj
+++ clojure/src/d07.clj
@@ -27,3 +27,46 @@
       (recur (conj order candidate) (disj remaining candidate) #{}))))
 
 ;; part 2
+(defn process-completed [{:as state :keys [time]}]
+  (reduce (fn complete-workers [next worker]
+            (if (= time (:done-at worker))
+              (-> next
+                  (update :done conj (:processing worker))
+                  (update :workers conj {}))
+              (update next :workers conj worker)))
+          (assoc state :workers [])
+          (:workers state)))
+
+(defn process-idle [{:as state :keys [time done workers remaining]}]
+  (if-let [candidates (not-empty (->> remaining sort (filter #(set/subset? (get graph % #{}) done))))]
+    (reduce (fn process-worker [next worker]
+              (if (empty? worker)
+                (let [[this & etc] (:candidates next)]
+                  (if this
+                    (-> next
+                        (update :remaining disj this)
+                        (assoc :candidates etc)
+                        (update :workers conj {:processing this
+                                               :done-at (+ time -4 (int (first (into (list) this))))}))
+                    (update next :workers conj {})))
+                (update next :workers conj worker)))
+            (merge state {:workers []
+                          :candidates candidates})
+            workers)
+    state))
+
+(let [steps (apply set/union (set (keys graph)) (vals graph))]
+  (loop [state
+         {:time -1
+          :remaining steps
+          :done #{}
+          :workers [{} {} {} {} {}]}]
+    (let [next (-> state
+                   (update :time inc)
+                   process-completed
+                   process-idle)]
+      (if (or (and (empty? (:remaining next))
+                   (empty? (remove empty? (:workers next))))
+              (< 1000000 (:time state)))
+        next
+        (recur next)))))
